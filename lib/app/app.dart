141016e1dfa570acea4d9a9c2a636e2816b1a1a9
yyupyong <yupyong.kim@gmail.com>
2023-07-14 10:08:31 +0900

    app.dartとして切り出し

---
+++ lib/app/app.dart
@@ -0,0 +1,19 @@
+import 'package:english_diary/view/home/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      //TODO:go_routerで実装
+      home: const HomeScreen(),
+    );
+  }
+}
