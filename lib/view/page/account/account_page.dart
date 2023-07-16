1a3a4a508f8895afa2453c1b994e71a339eaa7b1
yyupyong <yupyong.kim@gmail.com>
2023-07-16 10:08:17 +0900

    accountPageを追加

---
+++ lib/view/page/account/account_page.dart
@@ -0,0 +1,12 @@
+import 'package:english_diary/exporter/ui_exporter.dart';
+
+class AccountPage extends AbstractStatelessWidget {
+  const AccountPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      body: Center(child: Text("account")),
+    );
+  }
+}
