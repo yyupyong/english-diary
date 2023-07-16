08012ad982b9104b77eeff8684bfea3443690958
yyupyong <yupyong.kim@gmail.com>
2023-07-16 09:36:09 +0900

    abstractStatelesswidgetの作成

---
+++ lib/view/abstract_widget.dart
@@ -0,0 +1,6 @@
+export 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+abstract class AbstractStatelessWidget extends HookConsumerWidget {
+  const AbstractStatelessWidget({super.key});
+}
