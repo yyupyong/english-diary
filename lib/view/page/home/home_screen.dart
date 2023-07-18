c0eb00825557a35b303543e91019b1054629616f
yyupyong <yupyong.kim@gmail.com>
2023-07-19 08:25:17 +0900

    hommScreenにTabBarを移植

--- lib/view/page/home/home_screen.dart
+++ lib/view/page/home/home_screen.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:english_diary/exporter/ui_exporter.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends AbstractStatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final tabController = useTabController(initialLength: 2);
+
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        leading: Icon(Icons.dangerous),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(50),
+          child: TabBar(
+            controller: tabController,
+            tabs: const [
+              Tab(child: Text("おすすめ")),
+              Tab(child: Text("フォロー中")),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
