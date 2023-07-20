f010371f82cebd3aa684440cc27c9e1a6944944a
yyupyong <yupyong.kim@gmail.com>
2023-07-20 10:52:32 +0900

    検索ぺーじの追加

---
+++ lib/view/page/serch/serch_page.dart
@@ -0,0 +1,12 @@
+import 'package:english_diary/exporter/ui_exporter.dart';
+
+class SerchPage extends AbstractStatelessWidget {
+  const SerchPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const Center(
+      child: Text("data"),
+    );
+  }
+}
