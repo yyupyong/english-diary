cc8338cefb330163292e41515ab40b0612ffd80c
yyupyong <yupyong.kim@gmail.com>
2023-07-21 09:39:37 +0900

    デザイン諸々変更

--- lib/view/widget/home/diary_thread.dart
+++ lib/view/widget/home/diary_thread.dart
@@ -5,29 +5,29 @@ class DiaryThread extends AbstractStatelessWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Placeholder(
-      child: Container(
-        padding: EdgeInsets.symmetric(vertical: 16, horizontal: 24),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Row(
-              children: [],
+            Container(
+                margin: EdgeInsets.all(12),
+                width: 40,
+                height: 40,
+                child: Image.asset('assets/image/pngwing.com.png')),
+            Flexible(
+              child: Column(
+                children: [
+                  Text(
+                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+                ],
+              ),
             ),
-            Text(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
           ],
         ),
-      ),
+        const Divider(),
+      ],
     );
-  }
-}
-
-class UserDiaryItem extends AbstractStatelessWidget {
-  const UserDiaryItem({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return const Placeholder();
   }
 }
