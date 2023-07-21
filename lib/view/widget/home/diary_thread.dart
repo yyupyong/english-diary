733cf01d65878148e8557ead311df387d1a162da
yyupyong <yupyong.kim@gmail.com>
2023-07-21 09:46:01 +0900

    threadのUI大枠完成

--- lib/view/widget/home/diary_thread.dart
+++ lib/view/widget/home/diary_thread.dart
@@ -8,23 +8,28 @@ class DiaryThread extends AbstractStatelessWidget {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-                margin: EdgeInsets.all(12),
-                width: 40,
-                height: 40,
-                child: Image.asset('assets/image/pngwing.com.png')),
-            Flexible(
-              child: Column(
-                children: [
-                  Text(
-                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
-                ],
+        Padding(
+          padding: EdgeInsets.only(top: 16, left: 20, right: 20),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                  width: 40,
+                  height: 40,
+                  child: Image.asset('assets/image/pngwing.com.png')),
+              SizedBox(
+                width: 8,
               ),
-            ),
-          ],
+              Flexible(
+                child: Column(
+                  children: [
+                    Text(
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
         const Divider(),
       ],
