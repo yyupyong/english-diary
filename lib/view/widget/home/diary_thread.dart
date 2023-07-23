435614a0e7b29609b868b5e043615bed4cccca51
yyupyong <yupyong.kim@gmail.com>
2023-07-23 10:24:51 +0900

    画像の横にユーザー名と日付を設定

--- lib/view/widget/home/diary_thread.dart
+++ lib/view/widget/home/diary_thread.dart
@@ -20,9 +20,16 @@ class DiaryThread extends AbstractStatelessWidget {
               SizedBox(
                 width: 8,
               ),
-              Flexible(
+              const Flexible(
                 child: Column(
                   children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text('@ユーザー名'),
+                        Text('20xx/x/x'),
+                      ],
+                    ),
                     Text(
                         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."),
                   ],
