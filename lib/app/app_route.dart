0f58e71f74cfd56c67d148a361e0aa5d522d950b
yyupyong <yupyong.kim@gmail.com>
2023-07-29 16:46:51 +0900

    routerを使わないように

--- lib/app/app_route.dart
+++
@@ -1,6 +0,0 @@
-import 'package:english_diary/view/page/home/home_screen.dart';
-import 'package:go_router/go_router.dart';
-
-final router = GoRouter(routes: [
-  GoRoute(path: HomeScreen.routePath, builder: (_, __) => const HomeScreen())
-]);
