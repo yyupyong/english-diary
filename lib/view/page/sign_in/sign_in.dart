93be0483c84ab0b6a14877934cc63e3957a0698b
yyupyong <yupyong.kim@gmail.com>
2023-08-03 18:52:38 +0900

    sign_page追加

---
+++ lib/view/page/sign_in/sign_in.dart
@@ -0,0 +1,14 @@
+import 'package:english_diary/exporter/ui_exporter.dart';
+
+class SignInPage extends AbstractStatelessWidget {
+  const SignInPage({super.key});
+
+  static const routePath = '/sign_in';
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
