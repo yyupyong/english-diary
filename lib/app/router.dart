f4f7ce1f801f3fabe9ea28d4ec2073c6f6cb722c
yyupyong <yupyong.kim@gmail.com>
2023-08-03 18:53:56 +0900

    go_routerにsign_in_pageを追加

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -1,7 +1,12 @@
 import 'package:english_diary/view/page/bottom_navigation_page.dart';
+import 'package:english_diary/view/page/sign_in/sign_in.dart';
 import 'package:go_router/go_router.dart';
 
 final router = GoRouter(routes: [
+  GoRoute(
+    path: SignInPage.routePath,
+    builder: (_, __) => const SignInPage(),
+  ),
   GoRoute(
     path: BottomNavigationPage.routePath,
     builder: (_, __) => const BottomNavigationPage(),
